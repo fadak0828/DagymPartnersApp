5de5798006de631c9f41beff83a9b05c9b7538aa
fadak0828 <asd0828@gmail.com>
2019-01-09 14:20:41 +0900

    .

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, dialog } = require('electron');
 const { autoUpdater } = require('electron-updater');
 const path = require('path')
 const url = require('url')
@@ -33,7 +33,7 @@ function createWindow () {
 // Create window on electron intialization
 app.on('ready', function() {
     createWindow();
-    autoUpdater.checkForUpdates();
+    autoUpdater.checkForUpdatesAndNotify();
 })
 
 // Quit when all windows are closed.
@@ -52,8 +52,3 @@ app.on('activate', function () {
   }
 })
 
-// when the update has been downloaded and is ready to be installed, notify the BrowserWindow
-autoUpdater.on('update-downloaded', (info) => {
-    alert('최신 버전으로 업데이트합니다.');
-    autoUpdater.quitAndInstall();
-});
