7268a3d402b407cfc84812f3b992220819c6a3a7
fadak0828 <asd0828@gmail.com>
2019-01-09 11:24:39 +0900

    initial commit

---
+++ main.js
@@ -0,0 +1,63 @@
+const { app, BrowserWindow } = require('electron');
+const { autoUpdater } = require('electron-updater');
+const path = require('path')
+const url = require('url')
+
+let win;
+
+function createWindow () {
+  // Create the browser window.
+  win = new BrowserWindow({
+    width: 1080,
+    height: 920,
+    backgroundColor: '#ffffff',
+    allowEval: true,
+    webPreferences: {
+      nodeIntegration: false
+    },
+    icon: path.join(__dirname, 'resources/installer/icon.png')
+  })
+
+
+  win.loadURL('https://partners.da-gym.net');
+
+  //// uncomment below to open the DevTools.
+  // win.webContents.openDevTools()
+
+  // Event when the window is closed.
+  win.on('closed', function () {
+    win = null
+  })
+}
+
+// Create window on electron intialization
+app.on('ready', function() {
+    createWindow();
+    autoUpdater.checkForUpdates();
+})
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+
+  // On macOS specific close process
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', function () {
+  // macOS specific close process
+  if (win === null) {
+    createWindow()
+  }
+})
+
+// when the update has been downloaded and is ready to be installed, notify the BrowserWindow
+autoUpdater.on('update-downloaded', (info) => {
+    win.webContents.send('updateReady')
+});
+
+// when receiving a quitAndInstall signal, quit and install the new version ;)
+ipcMain.on("quitAndInstall", (event, arg) => {
+    autoUpdater.quitAndInstall();
+})
